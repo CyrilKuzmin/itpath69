c08831b631b0600c8fc611af202ac0174b67217f
Kirill Kuzmin <Kirll_Kuzmin@epam.com>
2022-05-21 21:00:38 +0300

    app

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"github.com/CyrilKuzmin/itpath69/app"
 	"github.com/CyrilKuzmin/itpath69/config"
-	"github.com/CyrilKuzmin/itpath69/service"
 )
 
 func main() {
 	conf := config.Get()
-	s := service.NewApp(conf)
-	s.Start()
+	a := app.NewApp(conf)
+	a.Start()
 }
